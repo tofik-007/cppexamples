b7a86806cc4f8d307d6b294ef07b61434891564b
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2021-06-22 12:35:28 +0530

    Update area of rectangle.cpp

--- area of rectangle.cpp
+++ area of rectangle.cpp
@@ -1,4 +1,5 @@
 //Take the length(L) and breadth(B) of the rectangle as input and find its area.
+https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118781/offering/1380851?leftPanelTab=0
 #include<iostream>
 using namespace std;
 int main(){
