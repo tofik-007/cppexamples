70c885cad303a4dff11a2d18fb46c60027d40af9
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2021-06-22 12:53:51 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //swapping of 2 number!
+
+using namespace std;
+
+int main()
+{   int a,b,swap;
+    cin>>a>>b;
+    swap = a;
+    a = b;
+    b = swap;
+    cout<<"value of a is "<<a<<endl;
+    cout<<"value of b is "<<b<<endl;
+    return 0;
+}
+   
