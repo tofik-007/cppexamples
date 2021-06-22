54943a3f74cf4648a474f809df889207c94af99e
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2021-06-22 12:31:38 +0530

    Create area of rectangle.cpp

---
+++ area of rectangle.cpp
@@ -0,0 +1,10 @@
+//Take the length(L) and breadth(B) of the rectangle as input and find its area.
+#include<iostream>
+using namespace std;
+int main(){
+  int l,b,area;
+  cin>>l,b;
+  area=l*b;
+  cout<<area;
+  return 0;
+}
