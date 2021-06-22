e942a49d41c6d79780d511148e9d1930c36c2b59
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2021-06-22 12:44:36 +0530

    Update name-age.cpp

--- name-age.cpp
+++ name-age.cpp
@@ -1,4 +1,4 @@
-Take the person's name and age as input and print out the name and age as specified in the output format.
+//Take the person's name and age as input and print out the name and age as specified in the output format.
 using namespace std;
 
 int main()
